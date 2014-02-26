20ff92f8b2b24d634d67ee51cbdbc4fdb0402a2f
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-02-26 22:32:03 +0800

    Check and uncheck me bitch

--- js/main.js
+++ js/main.js
@@ -4,18 +4,29 @@
     function init () {
         var todo = document.getElementById( 'form' );
         
-        todo.onsubmit = function ( e ) {
+        todo.addEventListener( 'submit', function ( e ) {
             e.preventDefault();
 
             var self = this;
 
             if ( document.getElementById( 'todo' ).value !== '' ) {
-                document.getElementById( 'todo-lists' ).innerHTML += '<li>' + '<input class=checkbox type=checkbox>' + self.todo.value + '</li>';
-                console.log( document.getElementById( 'todo-lists' ).querySelectorAll( 'li' ) );
+                document.getElementById( 'todo-lists' ).innerHTML += '<li>' + '<input type=checkbox>' + self.todo.value + '</li>';
+
+                var lists = document.getElementById( 'todo-lists' ).querySelectorAll( 'input' );
+
+                for ( var i = 0; i < lists.length; i ++ ) {
+                    lists[ i ].addEventListener( 'click', function( evt ) {
+                        if( this.checked ) {
+                            this.classList.add( 'checked' );
+                        } else {
+                            this.classList.remove( 'checked' );
+                        }
+                    } );
+                }
             }
 
             document.getElementById( 'todo' ).value = '';
-        };
+        } );
     }
 
     window.onload = function () {
