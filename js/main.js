90def04d7cd9afc713bf6bf4cc617d6b8bb9c695
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-02-26 11:43:26 +0800

    Modified index.html and main.js files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+( function () {
+    'use strict';
+
+    function init () {
+
+    }
+
+    window.onload = function () {
+        if ( document.readyState === 'complete' ) {
+            init();
+        }
+    }
+
+} )();
