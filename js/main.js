af44aa1e756430c14aff12c44760266137bc38e3
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-07-10 15:09:42 +0800

    Modified them files to be more readable

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 ( function () {
-    'use strict';
+	'use strict';
 
-    function init () {
-        var todo  = document.getElementById( 'form' );
-        var lists = document.getElementById( 'todo-lists' );
+	function init () {
+		var todo  = document.getElementById( 'form' );
+		var lists = document.getElementById( 'todo-lists' );
 
-        todo.addEventListener( 'submit', todoSubmit );
+		todo.addEventListener( 'submit', todoSubmit );
 
-        function todoSubmit ( e ) {
-            e.preventDefault();
+		function todoSubmit ( e ) {
+			e.preventDefault();
 
-            var todo     = document.getElementById( 'todo' );
-            var liTodo   = document.createElement( 'li' );
+			var todo     = document.getElementById( 'todo' );
+			var liTodo   = document.createElement( 'li' );
 
-            liTodo.innerHTML = '<input class="select" type="checkbox"><label>' + todo.value;
-            lists.appendChild( liTodo );
+			liTodo.innerHTML = '<input class="select" type="checkbox"><label>' + todo.value;
+			lists.appendChild( liTodo );
 
-            var liChecker = liTodo.querySelector( '.select' );
-            liChecker.addEventListener( 'click', todoChecker.bind( liChecker ) );
+			var liChecker = liTodo.querySelector( '.select' );
+			liChecker.addEventListener( 'click', todoChecker.bind( liChecker ) );
 
-            todo.value = '';
-        }
+			todo.value = '';
+		}
 
-        function todoChecker ( e ) {
-            if ( this.checked ) {
-                this.nextSibling.classList.add( 'checked' );
-            } else {
-                this.nextSibling.classList.remove( 'checked' );
-            }
-        }
-    }
+		function todoChecker ( e ) {
+			if ( this.checked ) {
+				this.nextSibling.classList.add( 'checked' );
+			} else {
+				this.nextSibling.classList.remove( 'checked' );
+			}
+		}
+	}
 
-    window.addEventListener( 'load', function () {
-        if ( document.readyState === 'complete' ) {
-            init();
-        }
-    } );
+	window.addEventListener( 'load', function () {
+		if ( document.readyState === 'complete' ) {
+			init();
+		}
+	} );
 
 } )();
