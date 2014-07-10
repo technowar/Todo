a6ccd8927c8c1be10021ea96d720faf02e0b351b
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-07-10 15:11:57 +0800

    Emptied js/main.js file

--- js/main.js
+++ js/main.js
@@ -2,33 +2,7 @@
 	'use strict';
 
 	function init () {
-		var todo  = document.getElementById( 'form' );
-		var lists = document.getElementById( 'todo-lists' );
-
-		todo.addEventListener( 'submit', todoSubmit );
-
-		function todoSubmit ( e ) {
-			e.preventDefault();
-
-			var todo     = document.getElementById( 'todo' );
-			var liTodo   = document.createElement( 'li' );
-
-			liTodo.innerHTML = '<input class="select" type="checkbox"><label>' + todo.value;
-			lists.appendChild( liTodo );
-
-			var liChecker = liTodo.querySelector( '.select' );
-			liChecker.addEventListener( 'click', todoChecker.bind( liChecker ) );
-
-			todo.value = '';
-		}
-
-		function todoChecker ( e ) {
-			if ( this.checked ) {
-				this.nextSibling.classList.add( 'checked' );
-			} else {
-				this.nextSibling.classList.remove( 'checked' );
-			}
-		}
+		alert( 'oop' );
 	}
 
 	window.addEventListener( 'load', function () {
